71f60100829ad611805dfea3cba804dfa3120553
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-10-09 01:19:39 +0300

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { ShowTotalNumbersComponent } from 'src/app/components/show-total-numbers/show-total-numbers.components';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthInfoComponent } from './components/auth-info/auth-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowTotalNumbersComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthInfoComponent
   ],
   exports: [
     ShowTotalNumbersComponent
@@ -29,6 +33,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatProgressBarModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
