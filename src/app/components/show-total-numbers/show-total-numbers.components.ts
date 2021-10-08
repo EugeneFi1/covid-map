656c88393c4f853ccb6ca003d46f995b53d730b1
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2021-10-09 00:31:46 +0300

    show total card

--- src/app/components/show-total-numbers/show-total-numbers.components.ts
+++ src/app/components/show-total-numbers/show-total-numbers.components.ts
@@ -1,23 +1,34 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit, Input } from "@angular/core";
 import { Observable, Subscription } from "rxjs";
 import { TotalNumberResponse } from "src/app/models/total-number-response.model";
 import { HttpRestService } from "src/app/services/http-rest.service";
 
 @Component({
     selector: 'show-total-numbers',
-    templateUrl: './show-total-numbers.component.html'
+    templateUrl: './show-total-numbers.component.html',
+    styleUrls: ['./show-total-numbers.component.less']
 })
 export class ShowTotalNumbersComponent implements OnInit, OnDestroy {
     constructor(private service: HttpRestService){
     }
     public subscription = new Subscription();
     public _totalNumber: number;
+    public _flagImageUrl: string;
+    public _location: string;
+    public loading: boolean;
+    @Input()
+    public countryCode: string;
+    @Input()
+    public country: string;
 
     public ngOnInit(): void {
-        this.subscription.add(this.service.loadUkraineTotalNumber()
+        this.loading = true;
+        this._flagImageUrl = `http://www.geonames.org/flags/x/${this.countryCode}.gif`;
+        this.subscription.add(this.service.loadTotalInfo(this.country)
             .subscribe((data: TotalNumberResponse) => {
                 this._totalNumber = data.data.confirmed;
-                console.log(data)
+                this._location = data.data.location;
+                this.loading = false;
             })
         );
     }
